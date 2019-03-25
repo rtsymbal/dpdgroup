babe8c8a6e82a439a298eefb56454afb08b0b3fb
Roman Tsymbal <roman.tsymbal@gmail.com>
2019-03-25 22:58:09 +0100

    Section 1 end

--- src/main/java/com/dpdgroup/NewUserCreation.java
+++
@@ -1,7 +0,0 @@
-package com.dpdgroup;
-
-public class NewUserCreation {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
--- src/test/java/com/dpdgroup/PayerSenderTest.java
+++
@@ -1,4 +0,0 @@
-package com.dpdgroup;
-
-public class PayerSenderTest {
-}
